5b25fe2b26f548c3c233b43181cd0a02ab4173fa
Matul Jain <matul3jan@gmail.com>
2023-04-13 10:11:08 +0000

    Updated Movies Endpoints

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
 import dotenv from 'dotenv';
 import express from 'express';
-import moviesRouter from './src/movies';
 import genresRouter from './src/genres';
-import createAccountsRouter from './src/accounts/routes';
 import buildDependencies from "./src/config/dependencies";
+import createAccountsRouter from './src/accounts/routes';
+import createMoviesRouter from './src/movies/routes';
 
 dotenv.config();
+
 const dependencies = buildDependencies();
-
 const app = express();
-
 const port = process.env.PORT;
 
 app.use(express.json());
-app.use('/api/movies', moviesRouter);
+app.use('/api/movies', createMoviesRouter(dependencies));
 app.use('/api/genres', genresRouter);
 app.use('/api/accounts', createAccountsRouter(dependencies));
 
