768575a0c8af572e8b69d2b9b83c855999d7a06d
Matul Jain <matul3jan@gmail.com>
2023-05-19 11:48:41 +0000

    Add route for test report

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import swaggerUi from 'swagger-ui-express';
 import winston from 'winston';
+import path from 'path';
 
 import openApiDocumentation from "./openapi.json";
 import buildDependencies from "./src/config/dependencies";
@@ -35,6 +36,12 @@ app.use('/api/movies', createMoviesRouter(dependencies));
 app.use('/api/genres', createGenresRouter(dependencies));
 app.use('/api/languages', createLanguagesRouter(dependencies));
 app.use('/api/artists', createArtistsRouter(dependencies));
+
+// Route to serve the test report
+app.get('/test-report', (req, res) => {
+    const filePath = path.join(__dirname, 'tests', 'test-report.html');
+    res.sendFile(filePath);
+});
 
 app.use(errorHandler); // Keep it last to catch all errors that may occur in the middleware stack
 
